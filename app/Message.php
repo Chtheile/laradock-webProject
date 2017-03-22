160e2a60498c943f932e9e3be930cbb7570b940f
Christian Theile <christian.theile@amcongmbh.de>
2017-03-22 17:16:31 +0100

    Database add chat message

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+
+    protected $fillable = ['message'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+    }
+
 }
